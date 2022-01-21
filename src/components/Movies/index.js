5c5960842135d408ca8a6cc22413ad078c265791
Jordyn <72579895+J0914@users.noreply.github.com>
2022-01-21 16:37:40 -0600

    first switch

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,20 @@
-function Movies() {
+import { Route, Switch, NavLink } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => (
+          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+        ))}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
